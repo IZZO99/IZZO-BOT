fe725435b32dfb976a0493f79fe7579781f881e0
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-07-13 20:11:03 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -12,10 +12,10 @@ module.exports = {
      .setFooter(`Request By | ${message.author.tag}`) 
      .setTitle(`list of all commands`)
      .setDescription(`[ Invite Bot ](https://discord.com/api/oauth2/authorize?client_id=816340990859673631&permissions=8&scope=bot)`)
-     .addField("🛠 Moderation", "`ban`, `kick`, `clear`, `mute`, `unmute`, `vmute`, `vunmute`, `lock`, `unlock`, `slowmode`, `vkick`, `unban`")
+     .addField("🛠 Moderation", "`ban`, `kick`, `clear`, `mute`, `unmute`, `vmute`, `vunmute`, `lock`, `unlock`, `slowmode`, `vkick`, `unban`, `addrole`,removerole`")
      .addField("ℹ️ Info", "`help`, `invite`, `ping`, `roles` ,`botinfo`, `userinfo`, `serverinfo`, `serveravatar`,  `avatar`")
-     .addField("😸Funny", "`cuddile`, `feed`, `hug`, `kiss`, `pat`, `poke`, `slap`,")
-     .addField("🤖Game", "`8ball`, `iq`, `rate`, respect`, `rps`, `ship`, `slots`,")
+     .addField("😸Funny", "`cuddile`, `feed`, `hug`, `kiss`, `pat`, `poke`, `slap`")
+     .addField("🤖Game", "`8ball`, `iq`, `rate`, respect`, `rps`, `ship`, `slots`")
      message.channel.send(embed);
     }
 }
