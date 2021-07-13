7229d0c7e4a2260267577889b0a6e9e3ac02d585
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-14 02:18:11 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,13 +11,12 @@ module.exports = {
      .setColor("RANDOM")
      .setFooter(`Request By | ${message.author.tag}`) 
      .setTitle(`list of all commands`)
-     .setDescription(`[ Invite Bot ](https://discord.com/api/oauth2/authorize?client_id=816340990859673631&permissions=8&scope=bot)`)
-     .addField("")
+     .setDescription(`[ Invite Bot ](https://discord.com/api/oauth2/authorize?client_id=816340990859673631&permissions=8&scope=bot)\n
        \`🛠Moderation\`,\`ban`,`kick`,`clear`,`mute`,`unmute`,`vmute`,`vunmute`,`lock`,`unlock`,`slowmode`,`vkick`,`unban`,`addrole`,`removerole`,
        \`ℹ️ Info\`,\`help\`,\`invite\`,\`ping\`,\`roles\`,\`botinfo\`,\`userinfo\`,\`serverinfo\`,\`serveravatar\`,\`avatar\`,
        \`😸Funny\`,\`cuddile\`,\`feed\`,\`hug\`,\`kiss\`,\`pat\`,\`poke\`,\`slap\`,
        \`🤖Game\`,\`8ball\`,\`iq\`,\`rate\`,\`respectw\`,\`rps\`,\`ship\`,\`slots\`,
-       \`🏙Images\`,\`captcha\`,\`love\`,\`phcomment\`,\`triggered\`,\`invert\`,\`wasted\`,\`clyde\`,\`facepalm\`,
+       \`🏙Images\`,\`captcha\`,\`love\`,\`phcomment\`,\`triggered\`,\`invert\`,\`wasted\`,\`clyde\`,\`facepalm\``)
      message.channel.send(embed);
     }
 }
